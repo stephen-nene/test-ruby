2ac8fe5140672f84e8550e6b4e2be7efcfb7ebc8
+stephen.nene <+>
2023-02-03 13:32:10 +0300

    more prints

--- app.rb
+++ app.rb
@@ -1,1 +1,20 @@
 puts "Hello world in ruby"
+
+# puts is used to print
+
+# print "Hello world!";
+
+# print does not add a line break after each statement
+
+#these two are similar
+puts [1, 2, 3].inspect
+p [1, 2, 3]
+puts [1, 2, 3] #turns data to a string
+
+
+# preaty print (objectify)
+puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+
